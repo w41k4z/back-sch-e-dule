9433e76769389842793d1496f4d18d87a91fb193
w41k4z <alainricor@gmail.com>
2023-03-06 21:52:01 +0000

    first commit

---
+++ src/main/java/com/hiu/backschedule/BackSchEDuleApplication.java
@@ -0,0 +1,13 @@
+package com.hiu.backschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSchEDuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSchEDuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiu/backschedule/BackSchEDuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiu.backschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSchEDuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
